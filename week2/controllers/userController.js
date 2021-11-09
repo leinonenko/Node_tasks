d1b59de20cafe3fad879f6ca61352569d75911b4
leinonenko <Kabaman1995p>
2021-11-09 09:18:21 +0200

    Adding some changes to gitignore file

--- week2/controllers/userController.js
+++ week2/controllers/userController.js
@@ -3,6 +3,41 @@
 
 const { users, getUser } = require('../models/userModel')
 
+/*
+const user_list_get = (req, res) => {
+  users.map((user)=>{
+    delete user.password;
+    return user;
+  });
+  res.json(users);
+};
+
+const user_get = (req, res) => {
+  const user = getUser(req.params.userId);
+  delete  user.password;
+  res.json(user);
+}
+// TODO post!
+const user_post = async (req, res) => {
+  console.log('add user data', req.body);
+  console.log('filename', req.file)
+  const user = req.body;
+  user.filename = req.file.filename;
+  user.message = `User added with id: ${await insertUser(user)}`;
+  res.json({message:`User created with id: ${id}`});
+}
+
+const user_delete = async (req, res) => {
+  const deleted = await deleteUser(req.params.userId);
+  res.json({message:`User deleted: ${deleted}`});
+};
+
+const user_update = async (req, res) => {
+  const updated = await updateUser(req.body);
+  res.json({message: `User updated:${updated}`});
+}
+*/
+
 const user_list_get = (req, res) => {
   users.map((user)=>{
     delete user.password;
--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -1,8 +1,18 @@
 'use strict';
 // userRoute
 const express = require('express');
-const {user_get, user_list_get, user_post} = require('../controllers/userController');
+const {user_get, user_list_get, user_post, user_update, user_delete} = require('../controllers/userController');
 const router = express.Router();
+/*
+router.route('/')
+.get(user_list_get)
+.post( upload.single('user'), user_post)
+.put(user_update);
+
+router.route('/:userId')
+.get(user_get)
+.delete(user_delete);
+*/
 
 router.get('/',user_list_get);
 
