a0ec5a573d5138df3cb58b3063af149c63c14d6c
leinonenko <Kabaman1995p>
2021-11-18 18:15:41 +0200

    passport,jwt and webtoken installation

---
+++ week2/controllers/authController.js
@@ -0,0 +1,12 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const login = (req, res) => {
+  // TODO: add passport authenticate
+};
+
+module.exports = {
+  login,
+};
+
--- week2/models/userModel.js
+++ week2/models/userModel.js
@@ -64,10 +64,24 @@ const updateUser = async (user) => {
   }
 };
 
+const getUserLogin = async (params) => {
+  try {
+    console.log(params);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE email = ?;',
+        params);
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
+
+
 module.exports = {
   getAllUsers,
   getUser,
   insertUser,
   deleteUser,
   updateUser,
+  getUserLogin
 };
